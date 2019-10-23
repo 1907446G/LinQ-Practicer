875663f8709accdf3132c62027143a36b3aaad04
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2019-10-23 22:56:52 +0800

    Creation of Dictionary and finding input using a key

--- Remake/Program.cs
+++ Remake/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,13 +23,39 @@ namespace Remake
 
                 var query = (from b in json
                              join p in CR on b.CurrencyRateID equals p.CurrencyRateID
-                             select new { Old = b.CurrencyRateID, New = p.CurrencyRateID });
-                
+                             orderby b.ToCurrencyCode
+                             group b by b.ToCurrencyCode into FX
+                             select new { Source = FX.First(), DestinationCurrency = FX.Key, AverageRate = FX.Average(x => x.AverageRate) });
+
+
+                var dict = new Dictionary<string, Decimal>();
+
                 foreach (var item in query)
                 {
-                    Console.WriteLine(item);
+                    dict.Add($"{item.Source.FromCurrencyCode}-{item.DestinationCurrency}", item.AverageRate);
                 }
+
+
+                foreach (var item in dict)
+                {
+                    Console.Write("Input the country code you want to change to (Example: USD): ");
+                    var filterCode = Console.ReadLine();
+                    if (item.Key.Contains(filterCode))
+                    {
+                        Console.WriteLine($"{item.Key}: {item.Value}");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cannot find the FX code entered. Please try again.");
+                        continue;
+                    }
+                }
+
+
+
                 Console.ReadKey();
+
             }
         }
     }
